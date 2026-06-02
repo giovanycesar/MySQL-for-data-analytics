5e18a26904d2b9cb9e811c4ddded10b8bdd0dee1
Giovany Almeida <gvncesar@gmail.com>
2026-06-02 19:57:58 -0300

    Standardize Data

--- Data Cleaning/Data Cleaning.sql
+++ Data Cleaning/Data Cleaning.sql
@@ -25,3 +25,125 @@ WHERE sweepstake_id IN (
 			sweepstake_id, ROW_NUMBER() OVER(PARTITION BY customer_id ORDER BY customer_id) AS row_num
 		FROM
 			bakery.customer_sweepstakes) AS table_row WHERE row_num>1);
+            
+SELECT 
+    phone, REGEXP_REPLACE(phone, '[()-/+]', '')
+FROM
+    customer_sweepstakes;
+    
+UPDATE customer_sweepstakes 
+SET 
+    phone = REGEXP_REPLACE(phone, '[()-/+]', '');
+    
+SELECT 
+    phone,
+    CONCAT(SUBSTRING(phone, 1, 3),
+            '-',
+            SUBSTRING(phone, 4, 3),
+            '-',
+            SUBSTRING(phone, 7, 4))
+FROM
+    customer_sweepstakes
+WHERE
+    phone <> '';
+    
+UPDATE customer_sweepstakes 
+SET 
+    phone = CONCAT(SUBSTRING(phone, 1, 3),
+            '-',
+            SUBSTRING(phone, 4, 3),
+            '-',
+            SUBSTRING(phone, 7, 4))
+WHERE
+    phone <> '';
+    
+SELECT 
+    phone
+FROM
+    customer_sweepstakes;
+    
+SELECT birth_date,
+       STR_TO_DATE(birth_date, '%m/%d/%Y'),
+       STR_TO_DATE(birth_date, '%Y/%d/%m')
+FROM customer_sweepstakes;
+
+SELECT 
+    birth_date,
+    IF(STR_TO_DATE(birth_date, '%m/%d/%Y') IS NOT NULL,
+        STR_TO_DATE(birth_date, '%m/%d/%Y'),
+        STR_TO_DATE(birth_date, '%Y/%d/%m'))
+FROM
+    customer_sweepstakes;
+    
+# Error
+
+UPDATE customer_sweepstakes 
+SET 
+    birth_date = IF(STR_TO_DATE(birth_date, '%m/%d/%Y') IS NOT NULL,
+        STR_TO_DATE(birth_date, '%m/%d/%Y'),
+        STR_TO_DATE(birth_date, '%Y/%d/%m'));
+        
+# Error 2
+
+UPDATE customer_sweepstakes 
+SET 
+    birth_date = CASE
+        WHEN STR_TO_DATE(birth_date, '%m/%d/%Y') IS NOT NULL THEN STR_TO_DATE(birth_date, '%m/%d/%Y')
+        WHEN STR_TO_DATE(birth_date, '%m/%d/%Y') IS NULL THEN STR_TO_DATE(birth_date, '%Y/%d/%m')
+    END;
+    
+SELECT 
+    birth_date,
+    CONCAT(SUBSTRING(birth_date, 9, 2),
+            '/',
+            SUBSTRING(birth_date, 6, 2),
+            '/',
+            SUBSTRING(birth_date, 1, 4))
+FROM
+    customer_sweepstakes;
+    
+    
+UPDATE customer_sweepstakes 
+SET 
+    birth_date = CONCAT(SUBSTRING(birth_date, 9, 2),
+            '/',
+            SUBSTRING(birth_date, 6, 2),
+            '/',
+            SUBSTRING(birth_date, 1, 4))
+WHERE
+    sweepstake_id IN (9 , 11);
+    
+SELECT 
+    *
+FROM
+    customer_sweepstakes;
+
+UPDATE customer_sweepstakes 
+SET 
+    birth_date = STR_TO_DATE(birth_date, '%m/%d/%Y');
+
+SELECT 
+    `Are you over 18?`
+FROM
+    customer_sweepstakes;
+
+SELECT `Are you over 18?`,
+       CASE
+           WHEN `Are you over 18?` = 'Yes' THEN 'Y'
+           WHEN `Are you over 18?` = 'No' THEN 'N'
+           ELSE `Are you over 18?`
+       END
+FROM customer_sweepstakes;
+
+UPDATE customer_sweepstakes 
+SET 
+    `Are you over 18?` = CASE
+        WHEN `Are you over 18?` = 'Yes' THEN 'Y'
+        WHEN `Are you over 18?` = 'No' THEN 'N'
+        ELSE `Are you over 18?`
+    END;
+    
+SELECT 
+    *
+FROM
+    customer_sweepstakes;
