42df8eeba2cb159a91ba456afb1748ce4e138393
Giovany Almeida <gvncesar@gmail.com>
2026-06-09 13:29:25 -0300

    US Household Income exploratory data analysis

--- Projects/US Household Income.sql
+++ Projects/US Household Income.sql
@@ -92,3 +92,100 @@ FROM
     us_project.us_household_income
 WHERE
     ALand = 0 OR ALand = '' OR ALand IS NULL;
+    
+SELECT 
+    State_Name, County, City, ALand, AWater
+FROM
+    us_household_income;
+
+SELECT 
+    State_Name,
+    SUM(ALand) AS total_land,
+    SUM(AWater) AS total_water
+FROM
+    us_project.us_household_income
+GROUP BY State_Name
+ORDER BY total_land DESC
+LIMIT 10;
+
+SELECT 
+    *
+FROM
+    us_project.us_household_income u
+        JOIN
+    us_project.us_household_income_statistics us ON u.id = us.id;
+    
+SELECT 
+    *
+FROM
+    us_project.us_household_income u
+        RIGHT JOIN
+    us_project.us_household_income_statistics us ON u.id = us.id
+WHERE u.id IS NULL;
+
+SELECT 
+    *
+FROM
+    us_project.us_household_income u
+        JOIN
+    us_project.us_household_income_statistics us ON u.id = us.id
+WHERE Mean <> 0;
+
+SELECT u.State_Name, 
+       u.County, 
+       u.Type, 
+       u.`Primary`, 
+       us.Mean, 
+       us.Median
+FROM us_household_income u
+INNER JOIN us_household_income_statistics us
+    ON u.id = us.id
+WHERE us.Mean <> 0;
+
+SELECT 
+    u.State_Name,
+    ROUND(AVG(us.Mean), 1),
+    ROUND(AVG(us.Median), 1)
+FROM
+    us_household_income u
+        INNER JOIN
+    us_household_income_statistics us ON u.id = us.id
+WHERE
+    us.Mean <> 0
+GROUP BY u.State_Name
+ORDER BY 2 DESC
+LIMIT 10;
+
+SELECT 
+    u.Type,
+    COUNT(u.Type),
+    ROUND(AVG(us.Mean), 1),
+    ROUND(AVG(us.Median), 1)
+FROM
+    us_household_income u
+        INNER JOIN
+    us_household_income_statistics us ON u.id = us.id
+WHERE
+    us.Mean <> 0
+GROUP BY 1
+HAVING COUNT(u.Type) > 100
+ORDER BY 4 DESC;
+
+SELECT 
+    *
+FROM
+    us_household_income
+WHERE
+    Type = 'Community';
+    
+SELECT 
+    u.State_Name,
+    u.City,
+    ROUND(AVG(us.Mean), 1) AS avg_mean_income,
+    ROUND(AVG(us.Median), 1) AS avg_median_income
+FROM
+    us_household_income u
+        JOIN
+    us_household_income_statistics us ON u.id = us.id
+GROUP BY u.State_Name , u.City
+ORDER BY 3 DESC;
